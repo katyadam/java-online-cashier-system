472609ae85d20ceaeec5880974154089ad71f449
Adam Kattan <kattanadam7@gmail.com>
2024-02-06 16:20:31 +0100

    adding cors policy for prod FE

--- src/main/java/com/example/onlinecashiersystem/config/CorsConfig.java
+++ src/main/java/com/example/onlinecashiersystem/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("http://localhost:5174", "https://ocs.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }

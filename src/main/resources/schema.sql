227b6f6b95d03ea4c4c23d99ebd6a193cf095b46
Adam Kattan <kattanadam7@gmail.com>
2024-02-12 12:26:44 +0100

    db update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,62 +3,85 @@ create sequence product_id_seq
 
 alter sequence product_id_seq owner to postgres;
 
-create table if not exists "user"
+create sequence token_id_seq
+    as integer;
+
+alter sequence token_id_seq owner to postgres;
+
+create table "user"
 (
     user_id       serial
-    primary key,
+        primary key,
     given_name    varchar(255),
     family_name   varchar(255),
     email         varchar(255)
-    unique,
-    password_hash varchar(255)
-    );
+        unique,
+    password_hash varchar(255),
+    role          varchar
+);
 
 alter table "user"
     owner to postgres;
 
-create table if not exists product_plane
+create table product_plane
 (
     product_plane_id serial
-    primary key,
+        primary key,
     name             varchar(255),
     fk_user_id       integer
-    constraint product_plane___fk
-    references "user",
+        constraint product_plane___fk
+            references "user",
     creation_date    date default CURRENT_TIMESTAMP
-    );
+);
 
 alter table product_plane
     owner to postgres;
 
-create table if not exists product
+create table product
 (
     product_id          integer default nextval('product_id_seq'::regclass) not null
-    primary key,
+        primary key,
     name                varchar(255),
     fk_product_plane_id integer
-    constraint product_product_plane_product_plane_id_fk
-    references product_plane,
+        constraint product_product_plane_product_plane_id_fk
+            references product_plane,
     price               double precision,
     currency            varchar
-    );
+);
 
 alter table product
     owner to postgres;
 
 alter sequence product_id_seq owned by product.product_id;
 
-create table if not exists transaction
+create table transaction
 (
     transaction_id serial
-    primary key,
+        primary key,
     record         varchar,
     fk_user_id     integer not null
-    constraint transaction_user_user_id_fk
-    references "user",
+        constraint transaction_user_user_id_fk
+            references "user",
     creation_time  timestamp default CURRENT_TIMESTAMP
 );
 
 alter table transaction
+    owner to postgres;
+
+create table token
+(
+    token_id   integer default nextval('token_id_seq'::regclass) not null
+        constraint token_pk
+            primary key,
+    token      varchar                                           not null,
+    token_type varchar                                           not null,
+    fk_user_id integer                                           not null
+        constraint token_user_fk
+            references "user",
+    expired    boolean                                           not null,
+    revoked    boolean
+);
+
+alter table token
     owner to postgres;
 
